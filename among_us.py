a56af69ea1041dfba7490b68dc6af19415b20267
Charlie Stratmann <stratmanncharlie@gmail.com>
2022-09-03 15:16:15 +1000

    Hugo Mayo is russian scum

---
+++ among_us.py
@@ -0,0 +1,9 @@
+weight = float(input("Input your weight: "))
+unit = input("(K)gs or (L)bs: ")
+match unit:
+    case "K":
+        solved = weight / 0.45
+        print(solved)
+    case "L":
+        solved2 = weight * 0.45
+        print(solved2)
